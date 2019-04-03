edf07c6bb30874f61b57eb31f122704df83123a7
LUIS GUSTAVO SANTOS <LUSANTOS@bbmapfre.com.br>
2019-04-03 19:25:18 -0300

    Cripto

--- src/App/index.js
+++ src/App/index.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
 import './App.css';
-import Welcome from './WelcomeMessage'
+import Welcome from './WelcomeMessage';
+import styled, { css, Button } from 'styled-components';
+
+const MyButton = styled.div` 
+  color: Tomato;
+  ${props => props.primary && css`
+    color: palevioletred;
+  `}
+`
+const TomatoButton = styled(MyButton)`
+  color: tomato;
+  border-color: tomato;
+`;
 
 class App extends Component {
   render() {
     return (
-      <Welcome name={"Welcome CryptoDash"} />
+      <div>
+        <Welcome name={"Welcome CryptoDash"} />
+        
+      </div>
     );
   }
 }
