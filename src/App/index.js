5159157a6ac9550291fe82b644e4c41facc28f7a
LUIS GUSTAVO SANTOS <LUSANTOS@bbmapfre.com.br>
2019-04-03 19:41:46 -0300

    AppBar

--- src/App/index.js
+++ src/App/index.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+
 import Welcome from './WelcomeMessage';
 import ApplyLayout from './AppLayout'
+import AppBar from './AppBar'
 
 class App extends Component {
   render() {
     return (
       <ApplyLayout>
+        <AppBar />
         <Welcome name={"Welcome to CryptoDash"} />
         
       </ApplyLayout>
