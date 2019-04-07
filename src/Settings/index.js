dda1c6eeeff6ab5e9c19f659acb241cfe1a3891f
Luis Gustavo Santos <spartadev@spartas-MacBook-Pro.local>
2019-04-07 12:22:04 -0300

    CoinGrid mostrando dados na tela

--- src/Settings/index.js
+++ src/Settings/index.js
@@ -2,10 +2,12 @@ import React from 'react'
 import Welcome from '../Settings/WelcomeMessage'
 import ConfirmButton from '../Settings/ConfirmButton'
 import Page from '../Shared/Page'
+import CoinGrid from './CoinGrid'
 
 export default function () {
     return <Page name="settings">
         <Welcome name={"Welcome to CryptoDash"} />
         <ConfirmButton />
+        <CoinGrid />
     </Page>
 }
